b116a0e5700de58081ec7e6d0f82f8701d13feb9
Sheldon Codling <codlinsh@gmail.com>
2023-10-17 23:49:21 -0400

    initial commit

---
+++ Credentials.h
@@ -0,0 +1,19 @@
+// Replace with your network credentials
+const char* SSID = // PUT WI-FI NETWORD SSID (NETWORK NAME) HERE
+const char* NETWORK_PASSWORD = // PUT WI-FI NETWORK PASSWORD HERE
+ 
+// SMTP information
+const String SMTP_HOST = // PUT THE SMTP HOST HERE 
+const int SMTP_PORT = 465;
+
+// Sign in credentials
+const String AUTHOR_EMAIL = // PUT SENDERS EMAIL ADDRESS HERE
+const String AUTHOR_PASSWORD = // PUT SENDERS EMAIL PASSWORD HERE
+
+// Recipients email
+const String RECIPIENT_EMAIL = // PUT THE EMAIL ADDRESS THAT YOU WOULD LIKE TO SEND AN EMAIL TO HERE
+
+// values for the min and max values for sensor
+// Need to adjust depending on the sensor in use
+const int dryest_value = 884;
+const int wettest_value = 420;
