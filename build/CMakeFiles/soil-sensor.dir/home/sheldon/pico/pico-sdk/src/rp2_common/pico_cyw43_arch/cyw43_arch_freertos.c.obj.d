ebf530f0d48455484feb4d4f37ebd3813fa510c2
codlingsh <codlinsh@gmail.com>
2024-05-17 02:17:55 -0400

    Removed build directory

--- build/CMakeFiles/soil-sensor.dir/home/sheldon/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/soil-sensor.dir/home/sheldon/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.obj: \
- /home/sheldon/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c
