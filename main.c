948464b05f0024f8b165166a4702c29a84096cb8
codlingsh <codlinsh@gmail.com>
2024-09-03 21:57:44 -0400

    Updated code

--- main.c
+++ main.c
@@ -24,6 +24,8 @@ void send_smtp_mail() {
     struct tm t;
     getDateNow(&t);
 
+    printf("The alarm has gone off and I have entered the function\n");
+
     char Date[100];
     strftime(Date, sizeof(Date), "%a, %d %b %Y %H:%M:%S %Z", &t);
     char message[] = "This email was sent at ";
@@ -32,20 +34,48 @@ void send_smtp_mail() {
     smtp_send_mail("sheldonspeppers@gmail.com", "5193284249@txt.bell.ca", "", message, my_smtp_result_fn, NULL);
 }
 
+// void test_callback() {
+//     printf("Alarm works!\n");
+// }
+
+char* set_hostname() {
+    char *result = (char *)malloc(20 * sizeof(char));
+    char board_id[8];
+    pico_unique_board_id_t uid;
+
+    pico_get_unique_board_id(&uid);
+    //printf("\n\n%02x\n", uid.id);
+    //printf("%d\n\n", uid.id);
+
+
+    if (result == NULL) {
+        return NULL;
+    }
+
+    result[0] = "\0";
+
+    sprintf(board_id, "%02X", uid.id);
+    strcat(result, board_id);
+
+    return result;
+}
+
 datetime_t *set_init_alarm_time(datetime_t *curr_time, datetime_t *alarm_time) {
     int final_day;
     bool is_leap_year = false;
 
+    
     alarm_time->year = curr_time->year;
-    alarm_time->hour = 12;
-    alarm_time->min = 0;
-    alarm_time->sec = 0;
+    alarm_time->dotw = curr_time->dotw;
+    alarm_time->hour = curr_time->hour;//12;
+    alarm_time->min = curr_time->min + 1;//0;
+    alarm_time->sec = 00;
 
     if (curr_time->year % 4 == 0 && (curr_time->year % 100 != 0 || curr_time->year % 400 == 0)) {
         is_leap_year = true;
     }
 
-    if (curr_time->hour >= 12) {
+    if (curr_time->hour >= 24 /* Should be 12 */) {
         // Determine final day of the month
         switch (curr_time->month) {
             case 2:
@@ -86,20 +116,12 @@ datetime_t *set_init_alarm_time(datetime_t *curr_time, datetime_t *alarm_time) {
 int main() {
 
     stdio_init_all();
-
-    pico_unique_board_id_t uid;
-    pico_get_unique_board_id(&uid);
-    printf("\n\n%02x\n", uid.id);
-    printf("%d\n\n", uid.id);
-
-    printf("\n\n\n");
-    printf("%x%x%x%x%x%x%x%x\n", uid.id[0],  uid.id[1], uid.id[2], uid.id[3], uid.id[4], uid.id[5], uid.id[6], uid.id[7]);
+    rtc_init();
     
     // Connect to Wifi and initialize TLS client for SMTP
-    connectToWiFi(country, ssid, pass, auth, "Soil-sensor", NULL, NULL, NULL);
+    connectToWiFi(country, ssid, pass, auth, set_hostname(), NULL, NULL, NULL);
     struct altcp_tls_config *tls_config = altcp_tls_create_config_client(NULL, 0);
     smtp_set_tls_config(tls_config);
-
 
     while (true)
     {
@@ -112,19 +134,21 @@ int main() {
 
     // Create 2 datetime variables to grab time and set alarm time based on current time.
     datetime_t t;
-    datetime_t alarm_time; 
     rtc_get_datetime(&t);
+    datetime_t alarm_time;
     set_init_alarm_time(&t, &alarm_time);
 
     smtp_set_server_addr("smtp.gmail.com");
     smtp_set_server_port(465);
     smtp_set_auth(email_address, email_password);
 
-    rtc_set_alarm(&alarm_time, *send_smtp_mail);
-    printf("current time is %d/%d/%d at %d:%02d:%02d\n", t.month, t.day, t.year, t.hour, t.min, t.sec);
-    printf("Alarm is set for %d/%d/%d at %d:%02d:%02d\n", alarm_time.month, alarm_time.day, alarm_time.year, alarm_time.hour, alarm_time.min, alarm_time.sec);
+    // Send initial email
+    //smtp_send_mail("sheldonspeppers@gmail.com", "5193284249@txt.bell.ca", "", "Soil Sensor has started", my_smtp_result_fn, NULL);
 
-    
+    rtc_set_alarm(&alarm_time, &send_smtp_mail);
+    printf("current time is %d/%d/%d at %d:%d:%d\n", t.month, t.day, t.year, t.hour, t.min, t.sec);
+    printf("Alarm is set for %d/%d/%d at %d:%d:%d\n", alarm_time.month, alarm_time.day, alarm_time.year, alarm_time.hour, alarm_time.min, alarm_time.sec);
+
     // Send initial email once system is started
     //smtp_send_mail("sheldonspeppers@gmail.com", "5193284249@txt.bell.ca", "", message, my_smtp_result_fn, NULL);
     
